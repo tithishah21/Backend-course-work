6799a91d2a4d53b8738b9f70ec2284bdb01a126a
Tithi Shah <shahtithi21@gmail.com>
2026-02-05 22:59:14 +0530

    parsing incoming requests

--- app.js
+++ app.js
@@ -8,14 +8,32 @@ const app = express();
 //initialize a new framework for us where express.js will store and see a lot of things for us
 //basically we create and express application
 
-app.use((req,res, next) => {
-    console.log("In the middleware");
-    next();  
-});
+// app.use((req,res, next) => {
+//     console.log("In the middleware");
+//     next();  
+// });
 //user goes to the website, middleware runs first and console prints "middleware" and the goes to the next route
-app.use((req,res, next) => {
+
+app.use('/',(req,res,next) => {
+    console.log("this always runs!!!");
+    next();
+});
+
+app.use('/addproduct',(req,res, next) => {
+    console.log("In another another middleware");
+    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    // next();  
+});
+
+app.use('/product',(req,res,next)=>{
+    console.log("product runs");
+    console.log(req.body);
+    res.redirect('/'); //we want to redirect this page to /
+});
+
+app.use('/',(req,res, next) => {
     console.log("In another middleware");
-    res.send("<h1>Server working woohoo!!!</h1> <i>Hello from express</i>");
+    res.send('<h1>Server working woohoo!!!</h1> <i>Hello from express</i>');
     next();  
 });
 //SO BASICALLY THE FLOW IS LIKE: request -> middleware -> route -> response
