4586d2eb7f70b6111c69a27033dd031c9c2654b6
Tithi Shah <shahtithi21@gmail.com>
2026-02-08 00:09:05 +0530

    made changes to shop.js file

--- routes/shop.js
+++ routes/shop.js
@@ -1,10 +1,16 @@
+const path = equire('path');
 const express = require('express');
 const router = express.Router();
 
 router.get('/',(req,res, next) => {
-    console.log("In another middleware");
-    res.send('<h1>Server working woohoo!!!</h1> <i>Hello from express</i>');
-     
+    // console.log("In another middleware");
+    // res.send('<h1>Server working woohoo!!!</h1> <i>Hello from express</i>');
+    res.sendFile(path.join(__dirname,'../','views', shop.html));
+    //join constructs a path by concatenating different segments
+    //dirname is a global variable that holds the absolute path on our OS to this project folder
+
+    //dirname will point to routes folder but views is a different folder
+    // "../" will help it to redirect it to move up 1 folder
 });
 
 module.exports = router;
