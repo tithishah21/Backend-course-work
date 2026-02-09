c6b69bd08d431b257fc7f3383323d2579d2fd842
Tithi Shah <shahtithi21@gmail.com>
2026-02-09 12:03:55 +0530

    __dirname...

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,10 @@
-
+const path = require('path');
 const express = require('express');
 const router = express.Router(); //express.js feature
 
 router.get('/addproduct',(req,res, next) => {
     console.log("In add product middleware");
-    res.send('<form action="/admin/addproduct" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>');
+    res.sendFile(path.join(__dirname,'../','views','addproduct.html'));
     // next();  
 });
 
