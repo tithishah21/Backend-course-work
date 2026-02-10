45988724b3cc424befa39c2dbf1852e1971048e8
Tithi Shah <shahtithi21@gmail.com>
2026-02-10 19:32:53 +0530

    404 html page redirected fxn - app.js

--- app.js
+++ app.js
@@ -7,13 +7,18 @@ const app = express();
 const adminRoutes= require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 //imported router object from admin.js
+
+const path = require('path');
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin',adminRoutes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).send('page not found');
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    //here we will not go to "../" as app.js is already in the root directory
 
+    // enter any random url now and it will redirect to 404.html page
 });
 
 app.listen(3000); 
