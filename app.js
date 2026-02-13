197110853a8816d8a036b48de4e6d8fa23be33f6
Tithi Shah <shahtithi21@gmail.com>
2026-02-13 22:37:31 +0530

    pagetitle now works perfectly for each page

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next)=>{
-    res.status(404).render('404');
+    res.status(404).render('404',{pageTitle: 'Page Not Found'});
 
     // res.status(404).sendFile(path.join(__dirname,'views','404.html'));
     //here we will not go to "../" as app.js is already in the root directory
