5b0ef2e6be8c6eeb2f52177140f134f24989983c
Tithi Shah <shahtithi21@gmail.com>
2026-02-16 19:10:21 +0530

    POST route as controller in products.js

--- controllers/products.js
+++ controllers/products.js
@@ -1,7 +1,14 @@
+const products = []; 
+
 exports.getAddProduct = (req,res, next) => {
 
     res.render('addproduct',
     {pageTitle: 'Add Product', 
     path:'/admin/addproduct', 
     activeAddProduct: true});
+}
+
+exports.postAddProduct = (req,res,next)=>{
+    products.push({title: req.body.title});
+    res.redirect('/'); 
 }
--- routes/admin.js
+++ routes/admin.js
@@ -1,29 +1,14 @@
 const path = require('path');
 const express = require('express');
 
-
 const productsController = require('../controllers/products');
 
-
-// const rootDir = require('../util/path');
-//our root directory
-
 const router = express.Router(); //express.js feature
-
-const products = []; //will store the objects we enter
 
 router.get('/addproduct',productsController.getAddProduct);
 //router.get is basically doing - whwnever we go to admin/addproduct , it will send us to a page addproduct.html.
 
-router.post('/addproduct',(req,res,next)=>{
-    // console.log("product runs / product middleware");
-    // console.log(req.body);
-    products.push({title: req.body.title});
-    res.redirect('/'); 
-});
+router.post('/addproduct',productsController.postAddProduct);
 //router.post does - when you click "Add Product", it will store the input that i typed "req.body" and print it into the server using console.log and then finally redirects us to / which shows shop.html
 
-// module.exports = router;
-
-exports.routes = router;
-exports.products = products;
+module.exports = router;
