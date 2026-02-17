531570ce9b58a86495db4bf5382e5a3677a4047b
Tithi Shah <shahtithi21@gmail.com>
2026-02-17 12:03:41 +0530

    added coments for clarity in product.js

--- models/product.js
+++ models/product.js
@@ -8,9 +8,11 @@ module.exports = class Product {
     save() {
         products.push(this);
         //this refers to the object created in our class
+        //each product gets pushed in the products array
     }
 
     static fetchAll() {
         return this.products;
+        //returns the names of all the products in the products array
     }
 }
