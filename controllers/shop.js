7da3250858b665b3e574c92aa5f67daafef18aca
Tithi Shah <shahtithi21@gmail.com>
2026-02-19 12:43:09 +0530

    deleted products.js and moved the code to shop.js inside controllers

---
+++ controllers/shop.js
@@ -0,0 +1,29 @@
+const Product = require('../models/product');
+
+// const products = []; 
+
+exports.getAddProduct = (req,res, next) => {
+
+    res.render('admin/addproduct',
+    {pageTitle: 'Add Product', 
+    path:'/admin/addproduct', 
+    activeAddProduct: true});
+}
+
+exports.postAddProduct = (req,res,next)=>{
+    const product = new Product(req.body.title);
+    product.save();
+    res.redirect('/'); 
+}
+
+exports.getProducts = (req,res, next) => {
+    const products = Product.fetchAll((products) => {
+        res.render('shop/product-list', {
+            prods: products,
+            hasProducts: products.length > 0,
+            pageTitle: 'Shop',
+            path: '/'
+        });  
+    });
+    
+}
