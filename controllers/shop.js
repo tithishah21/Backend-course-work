0a5f17573e046edc904f7cb1ddd6d301ac5be069
Tithi Shah <shahtithi21@gmail.com>
2026-02-21 13:14:22 +0530

    created cart and checkout controllers

--- controllers/shop.js
+++ controllers/shop.js
@@ -4,9 +4,33 @@ exports.getProducts = (req,res, next) => {
     const products = Product.fetchAll((products) => {
         res.render('shop/product-list', {
             prods: products,
-            hasProducts: products.length > 0,
+            pageTitle: 'All Products',
+            path: '/products',
+
+        });  
+    });
+}
+
+exports.getIndex = (req,res,next) => {
+    Product.fetchAll((products) => {
+        res.render('shop/index', {
+            prods: products,
             pageTitle: 'Shop',
             path: '/'
         });  
+    });
+}
+
+exports.getCart = (req,res,next) => {
+    res.render('shop/cart',{
+        path: '/cart',
+        pageTitle: 'Your Cart'
+    });
+}
+
+exports.getCheckout = (req,res,next)=> {
+    res.render('shop/checkout',{
+        path: '/checkout',
+        pageTitle: 'Ccheckout'
     });
 }
--- routes/shop.js
+++ routes/shop.js
@@ -6,12 +6,12 @@ const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', shopController.getProducts);
+router.get('/', shopController.getIndex);
 
-router.get('/products');
+router.get('/products',shopController.getProducts);
 
-router.get('/cart');
+router.get('/cart',shopController.getCart);
 
-router.get('/checkout');
+router.get('/checkout',shopController.getCheckout);
 
 module.exports = router;
