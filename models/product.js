553e21f72854a4708709875938d715371127ed67
Tithi Shah <shahtithi21@gmail.com>
2026-04-10 19:17:22 +0530

    Defining product model using Sequelize

--- models/product.js
+++ models/product.js
@@ -1,38 +1,30 @@
-const db = require('../util/database');
-const Cart = require('./cart');
+const Sequelize = require('sequelize');
 
-module.exports = class Product {
-    constructor(title, imageUrl, description, price, id){
-        this.title = title;
-        this.imageUrl = imageUrl;
-        this.description = description;
-        this.price = price;
-        this.id = id;
+const sequelize = require('../util/database');
+
+const Product = sequelize.define('product',{
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    price: {
+        type: Sequelize.DOUBLE,
+        allowNull: false
+    },
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    imageUrl: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
+});
 
-    save() {
-        if (this.id) {
-            return db.execute(
-                'UPDATE products SET title = ?, price = ?, description = ?, imageUrl = ? WHERE id = ?',
-                [this.title, this.price, this.description, this.imageUrl, this.id]
-            );
-        }
-
-        return db.execute(
-            'INSERT INTO products (title, price, description, imageUrl) VALUES (?, ?, ?, ?)',
-            [this.title, this.price, this.description, this.imageUrl]
-        );
-    }
-
-    static deleteById(id){
-        return db.execute('DELETE FROM products WHERE id = ?', [id]);
-    }
-
-    static fetchAll() {
-        return db.execute('SELECT * FROM products');
-    }
-
-    static findById(id) {
-        return db.execute('SELECT * FROM products WHERE id = ?', [id]);
-    }
-}
+module.exports = Product;
