c4063294b82377c045d5b406d75b4aefa4dffe4e
Tithi Shah <shahtithi21@gmail.com>
2026-04-10 20:01:30 +0530

    retrieving data from table, SHOP & PRODUCTS pages working

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,16 +2,15 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req,res, next) => {
-    Product.fetchAll()
-    .then(([products]) => {
+    Product.findAll().then(products => {
         res.render('shop/product-list', {
             prods: products,
             pageTitle: 'All Products',
-            path: '/products',
-
-        });  
-    })
-    .catch(err => console.log(err));
+            path: '/products' 
+        });
+    }).catch(err => {
+        console.log(err);
+    });
 }
 
 
